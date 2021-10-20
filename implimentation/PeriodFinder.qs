84a7602b4e29c083357781b50c6a5ccdf2f4baf8
Peter Oslington <peteroslington1@gmail.com>
2021-10-20 22:42:47 +1100

    Added basic implementation based heavily on Mariaa's code

--- implimentation/PeriodFinder.qs
+++ implimentation/PeriodFinder.qs
@@ -1,7 +1,67 @@
 namespace PeriodFinder {
     open Microsoft.Quantum.Intrinsic;
 
+    // open Microsoft.Quantum.Arithmetic;
+    // open Microsoft.Quantum.Math;
+    // open Microsoft.Quantum.Standard;
+    
+
+
+    // open Microsoft.Quantum.Convert;
+
+    open Microsoft.Quantum.Canon;
+
+
+    open Microsoft.Quantum.Diagnostics;
+    open Microsoft.Quantum.Arithmetic;
+    open Microsoft.Quantum.Convert;
+    open Microsoft.Quantum.Math;
+    open Microsoft.Quantum.Oracles;
+    open Microsoft.Quantum.Characterization;
+
     operation FindPeriod(a : Int, N : Int) : Int {
-        return 0;
+        return EstimatePeriod(a,N);
+    }
+
+    operation MultiplyMod(a : Int, N : Int, r : Int, target : Qubit[]) : Unit is Adj + Ctl {
+        MultiplyByModularInteger(ExpModI(a,r,N), N, LittleEndian(target));
+    } 
+
+    operation EstimatePeriod(a : Int, N : Int) : Int {
+        mutable result = 1;
+        let bitsize = BitSizeI(N);
+        let bitsprecision = 2 * bitsize + 1;
+
+        repeat {
+            mutable var = 0;
+
+            use eigenstateRegister = Qubit[bitsize];
+            let eigenstateRegisterLE = LittleEndian(eigenstateRegister);
+            ApplyXorInPlace(1, eigenstateRegisterLE);
+
+            let oracle = DiscreteOracle(MultiplyMod(a, N, _, _));
+
+            use register = Qubit[bitsprecision];
+            let var2 = LittleEndian(register);
+
+            QuantumPhaseEstimation(oracle, eigenstateRegisterLE!, LittleEndianAsBigEndian(var2));
+
+            set var = MeasureInteger(var2);
+
+            ResetAll(eigenstateRegister);
+
+            Message(IntAsString(var));
+
+            let (numerator, period) = (ContinuedFractionConvergentI(Fraction(var, 2 ^ bitsprecision), N))!;
+            let (numeratorAbs, periodAbs) = (AbsI(numerator), AbsI(period));
+
+            //Message($"Estimated divisor of period is {periodAbs}, " + $" we have projected on eigenstate marked by {numeratorAbs}.");
+
+            set result = (periodAbs * result) / GreatestCommonDivisorI(result, periodAbs);
+
+        }
+        until (ExpModI(a,result,N) == 1);
+
+        return result;
     }
 }
--- implimentation/driver.py
+++ implimentation/driver.py
@@ -1,4 +1,83 @@
+import sys
+import random
+from math import gcd
+
 import qsharp
 from PeriodFinder import FindPeriod
+from QSharp.Chapter12 import EstimatePeriod
 
-print(FindPeriod.simulate(a=2,N=15))
+
+def main():
+    N = readArgument()
+    print(f"Starting to factorise {N}...")
+    specialCases(N)
+    generalCase(N)
+    
+
+def readArgument():
+    # Pull in value to factoris from first CLI argument
+    if len(sys.argv) <= 1:
+        raise ValueError('Please provide a nubmer to factorise')
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        raise ValueError('Please provide an integer value to factorise')
+    return N
+
+def specialCases(N):
+    # check if N is even
+    print(f"Checking if {N} is even...")
+    if (N%2 == 0):
+        print(f"{N} is even!")
+        foundFactors(N,2,int(N/2))
+    print(f"{N} is not even. Checking if {N} is prime...")
+    # check if N is prime TODO
+    if False:
+        raise ValueError(f'{N} is prime and cannot be factored')
+    print(f"{N} is not prime. Starting general factorisation algorithm...")
+
+
+def generalCase(N):
+    while True:
+        a = random.randint(2,N-1)
+        print(f"Picked random cofactor {a}")
+        gcd_result = gcd(a,N)
+        if gcd_result > 1:
+            print(f"Random cofactor {a} is a factor of {N}!")
+            foundFactors(N, gcd_result, int(N/gcd_result))
+        print(f"Random cofactor {a} is not a factor of {N}. Starting factorisation...")
+        r = findOrder(a, N)
+        print(f"Order of {a} mod {N} is {r}.")
+        if (r%2 == 0):
+            print(f"Order {r} is even. Calculating a likely factor...")
+            x = (a**int(r/2) - 1) % N
+            print(f"Testing likely factor {x}...")
+            gcd_result = gcd(x,N)
+            if (gcd_result > 1):
+                foundFactors(N,gcd_result,int(N/gcd_result))
+            else:
+                print(f"Likely factor {x} was not a factor. Starting again with a new cofactor.")
+        else:
+            print(f"Order {r} is not even. Starting again with a new cofactor.")
+
+def findOrder(a, N):
+    result = None
+    while (not isinstance(result,int)):
+        return FindPeriod.simulate(a=a,N=N)
+        # result = (EstimatePeriod.simulate(generator=a,modulus=N))
+    return result
+
+def classicalOrderFinder(a, N):
+    r = 1
+    while True:
+        if (a**r)%N == 1:
+            return r
+        r += 1
+
+def foundFactors(N,p,q):
+    print(f"Factors of {N} found: {p}, {q}")
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
